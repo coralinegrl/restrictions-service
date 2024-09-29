f3f70295f06fbc69670d232e87e5479c71d565eb
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-09-29 15:21:24 -0300

    Comment auth.js file

--- config/firebase.js
+++ config/firebase.js
@@ -5,7 +5,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Importante para manejar correctamente los saltos de línea en la clave privada
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Manejar correctamente los saltos de línea en la clave privada
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -14,13 +14,12 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
 };
 
-// Inicializar Firebase Admin SDK con las credenciales cargadas desde las variables de entorno
+// Inicializar Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// Crear la referencia a Firestore
+// Crear una referencia a Firestore
 const db = admin.firestore();
 
-// Exportar la referencia a Firestore para usarla en otros archivos
 module.exports = db;
