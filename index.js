c4975c897ba5d9b3ae646ad66cefc7a75109b0b2
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2024-09-29 21:06:45 -0300

    Add Swagger to index.js

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const restrictionsRoutes = require('./routes/restrictions'); // Rutas del servicio de restricciones
 require('dotenv').config(); 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const app = express();
 
-// middleware para interpretar JSON en el cuerpo de las solicitudes
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Restrictions Service API',
+      version: '1.0.0',
+      description: 'API para gestionar restricciones de estudiantes'
+    },
+    servers: [
+      {
+        url: 'https://restrictions-service.onrender.com',
+
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(express.json());
 
-//rutas del servicio de restricciones
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido al servicio de restricciones');
+});
+
+// Rutas del servicio de restricciones
 app.use('/restrictions', restrictionsRoutes);
 
-
 const PORT = process.env.PORT || 3000;
-
 
 app.listen(PORT, () => {
   console.log(`Restrictions Service running on port ${PORT}`);
